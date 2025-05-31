e84876a291a3a69bee17a85391714c2413fd0881
Diaz647 <hostgeldihostgelii@gmail.com>
2025-05-31 16:48:02 +0400

    Update taze.py

--- taze.py
+++ taze.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__) # DÜZELTİLDİ: name -> __name__
 
 # --- Yapılandırma ---
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or '8128882254:AAEZ_6OicThy8hlo-k4JShBlsatOyqzRhBY' # Token'ınızı buraya girin
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or '7729428395:AAEVdRCIWy8QkADFliXyee5GXQH2qz_ESKM' # Token'ınızı buraya girin
 bot = telebot.TeleBot(TOKEN, parse_mode=None) # parse_mode'u daha sonra özel olarak ayarlayacağız
 
 # Webhook ayarları (Render.com için)
